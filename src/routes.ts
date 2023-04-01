92ae6db791b55e8f904d2d93fd234ec21e632f43
KANG DONGHA <knamuuu@naver.com>
2023-04-01 20:41:04 +0900

    Feat: lazy component 및 router.ts 추가

---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import { lazy } from 'react';
+
+export const home = lazy(() => import('./pages/Home'));
+
+export const hbti = lazy(() => import('./pages/Hbti'));
+export const hbtiResult = lazy(() => import('./pages/HbtiResult'));
