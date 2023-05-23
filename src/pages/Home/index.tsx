07d34cf16466374057a0c2126d2afd8ec09a5d72
yunmi099 <yunmi099@ajou.ac.kr>
2023-05-23 12:56:54 +0900

    Feat : home 레이아웃

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,8 +6,12 @@ import Header from '../../components/layout/header';
 function Home() {
     const navigate = useNavigate();
     return(
-    <div style={{width:"100vw", height:"100vh"}}>
-     <Header title="부동산상식"/>
+    <div className={styles.container}>
+     <div className={styles.topContainer}>
+        
+     </div>
+     <div className={styles.bottomContainer}>
+     </div> 
      <BottomTab/>
     </div>);
     // <div style={{textAlign:"center",width:"100vw", fontSize:30 }} onClick={()=>navigate('/hbti')}>hbti</div>;
