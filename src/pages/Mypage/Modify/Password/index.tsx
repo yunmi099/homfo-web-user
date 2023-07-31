e02438a6a805716fe8f2e5c4964d186a4958cdcf
yunmi099 <yunmi099@ajou.ac.kr>
2023-07-31 20:18:42 +0900

    Feat: 숫자키패드 먼저오도록 inputMod변경

--- src/pages/Mypage/Modify/Password/index.tsx
+++ src/pages/Mypage/Modify/Password/index.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Header from "../../../../components/layout/header";
 import styles from '../styles.module.scss'
 import { useNavigate } from "react-router-dom";
 const ModifyPassword = ()=>{
     const navigate = useNavigate();
+    const [password, setPassword]=useState<string>("");
     return(
     <div className={styles.container}>
         <Header title="비밀번호 변경" color="white"/>
         <div className={styles.inputBox}>
-            <input className={styles.input} type="password" placeholder="현재 비밀번호를 입력하세요."/>
-            <input className={styles.input} type="password" placeholder="새 비밀번호를 입력하세요."/>
-            <input className={styles.input} type="password" placeholder="새 비밀번호를 한번 더 확인해주세요."/>
+            <input className={styles.input} type="password" inputMode="numeric" placeholder="현재 비밀번호를 입력하세요."/>
+            <input className={styles.input} type="password" inputMode="numeric" placeholder="새 비밀번호를 입력하세요."/>
+            <input className={styles.input} type="password" inputMode="numeric" placeholder="새 비밀번호를 한번 더 확인해주세요."/>
         </div>
         <button className={styles.button}>비밀번호 변경</button>
         <div className={styles.textButton} onClick={()=>navigate(-1)}>다음에 변경하기</div>
--- src/pages/Mypage/Modify/Phonenumber/index.tsx
+++ src/pages/Mypage/Modify/Phonenumber/index.tsx
@@ -10,10 +10,10 @@ const ModifyPhonenumber = ()=>{
         <Header title="전화번호 변경" color="white"/>
         <div className={styles.inputBox}>
             <div>
-                <input className={styles.input} style={{width:'59vw'}}type="text" placeholder="변경할 전화번호를 입력하세요."/>
+                <input className={styles.input} style={{width:'59vw'}}  inputMode="numeric" type="text" placeholder="변경할 전화번호를 입력하세요."/>
                 <button style={{width:'28.5vw',height:'5.5vh'}}onClick={()=>setOpen(true)}>인증 요청</button>
             </div>
-            {open&&<input className={styles.input} type="text" placeholder="인증번호를 입력하세요"/>}
+            {open&&<input className={styles.input} type="text"  inputMode="numeric" placeholder="인증번호를 입력하세요"/>}
         </div>
         <button className={styles.button}>전화번호 변경</button>
         <div className={styles.textButton} onClick={()=>navigate(-1)}>다음에 변경하기</div>
