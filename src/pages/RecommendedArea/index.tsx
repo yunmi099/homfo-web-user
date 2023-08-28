c04f81459549875b98b6a2c082d494a71ed850d6
yunmi099 <yunmi099@ajou.ac.kr>
2023-08-29 01:00:45 +0900

    Feat: 홈포추천구역 시작하기 화면

--- src/pages/RecommendedArea/index.tsx
+++ src/pages/RecommendedArea/index.tsx
@@ -1,10 +1,14 @@
 import React,{useState, useEffect} from 'react';
 import styles from './styles.module.scss';
 import ProgressStepTracker from '../../components/progressStepTracker';
+import InitialPage from './InitialPage';
 function RecommendedArea(){
-    const [count, setCount] = useState(2);
+    const [count, setCount] = useState(0);
     return (<div className={styles.container}>
-    <ProgressStepTracker count={count} totalCount={6}/>
+    {count===0?
+    <InitialPage count={count} setCount={setCount}/>
+    :<ProgressStepTracker count={count} totalCount={6}/>
+    }    
     </div>)
 }
 export default RecommendedArea;
