0be4d9c853eea513d93ca669f0eac82555fb6ad6
yunmi099 <yunmi099@ajou.ac.kr>
2023-09-25 22:22:48 +0900

    Feat: 요청하기 custom modal

--- src/pages/Request/zoneSelectPage/index.tsx
+++ src/pages/Request/zoneSelectPage/index.tsx
@@ -1,18 +1,42 @@
-import React from "react";
-import styles from '../styles.module.scss'
-import KakaoMap from "../../KakaoMap";
+import React, { useEffect, useState } from "react";
 import ConfirmButton from "../../../components/button/ConfirmButton";
 import Header from "../../../components/layout/header";
-import Question from "../../../components/selecedProgress/question";
+import { getAreaInfo } from "../../../services/reauest/api";
+import styles from './styles.module.scss';
+import { Area } from "../../../store/type/request/interface";
+import check from '../../../assets/icons/request/checkBox.png';
+import CustomModal from "./modal";
+
+
 interface InitialPageProps{
     count: number;
     setCount: React.Dispatch<React.SetStateAction<number>>;
 }
+
 const ZoneSelectPage = (props: InitialPageProps)=>{
+    const [areaInfo, setAreaInfo] = useState<Area[]|undefined>();
+    const [selfChoice, setSelfChoice] = useState<boolean>(true);
+    const [modalIsOpen, setModalIsOpen] = useState(true);
+    getAreaInfo("단국대학교", "본교",setAreaInfo);
 return(<>
         <Header title="요청하기" color="white"/>
-        <Question question="<span>어느 구역</span>을 찾으실 건가요?"/>
-        <p>지도자리*^^*</p>
+        <div className={styles.container}>
+            <CustomModal modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />
+            <div className={styles.question}><span>어느 구역</span>을 찾으실 건가요?</div>
+            {areaInfo!==undefined&&
+            <>
+            <div className={styles.checkBox} >
+                <img src={check} width={25} height={25} alt="checkBox"/>
+                <div>제가 직접 고를게요</div>
+            </div>
+            {selfChoice&&
+            <div className={styles.mapContainer}>
+                {/* <>지도자리</> */}
+            </div>
+            }
+            </>}
+
+        </div>
         <ConfirmButton onClick={()=>props.setCount(props.count+1)} auth={true} title="다음"/>
 </>);
 }
---
+++ src/store/type/request/interface.ts
@@ -0,0 +1,8 @@
+export {type Area};
+interface Area {
+    areaId: number;
+    name: string;
+    radius: number;
+    lat: number;
+    lng: number;
+  }
