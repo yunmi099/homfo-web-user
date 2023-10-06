c5ccbac041b3e6af06d56397d16cc42e583e8da2
yunmi099 <yunmi099@ajou.ac.kr>
2023-10-07 03:27:26 +0900

    Design: 홈화면 아이콘 수정

--- src/assets/icons/bottomtab/bottomtab.ts
+++ src/assets/icons/bottomtab/bottomtab.ts
@@ -1,8 +1,8 @@
-export { default as Home } from '../../../assets/icons/bottomtab/home/home.svg';
-export { default as NHome } from '../../../assets/icons/bottomtab/home/nothome.svg';
-export { default as Map } from '../../../assets/icons/bottomtab/map/map.svg';
-export { default as NMap } from '../../../assets/icons/bottomtab/map/notmap.svg';
-export { default as Mypage } from '../../../assets/icons/bottomtab/mypage/mypage.svg';
-export { default as NMypage } from '../../../assets/icons/bottomtab/mypage/notmypage.svg';
-export { default as RequestBox } from '../../../assets/icons/bottomtab/requestBox/requestbox.svg';
-export { default as NRequestBox } from '../../../assets/icons/bottomtab/requestBox/notrequestbox.svg';
+export { default as Home } from '../../../assets/icons/bottomtab/home/home.png';
+export { default as NHome } from '../../../assets/icons/bottomtab/home/nothome.png';
+export { default as Map } from '../../../assets/icons/bottomtab/map/map.png';
+export { default as NMap } from '../../../assets/icons/bottomtab/map/notmap.png';
+export { default as Mypage } from '../../../assets/icons/bottomtab/mypage/mypage.png';
+export { default as NMypage } from '../../../assets/icons/bottomtab/mypage/notmypage.png';
+export { default as RequestBox } from '../../../assets/icons/bottomtab/requestBox/requestbox.png';
+export { default as NRequestBox } from '../../../assets/icons/bottomtab/requestBox/notrequestbox.png';
