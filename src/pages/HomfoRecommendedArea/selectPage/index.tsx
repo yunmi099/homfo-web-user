ac2aa065a2fadcfc214aff1ca50472998bdbd494
yunmi099 <yunmi099@ajou.ac.kr>
2023-10-10 03:02:55 +0900

    Rename: 변수명 hompo->homfo로 변경

--- src/pages/HomfoRecommendedArea/selectPage/index.tsx
+++ src/pages/HomfoRecommendedArea/selectPage/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { hompoQuestionList } from '../homfoQuestionList';
+import { homfoQuestionList } from '../homfoQuestionList';
 import ConfirmButton from '../../../components/button/ConfirmButton';
-import useHompoSurveyStore from '../../../store/context/useHomfoSurveyStore';
+import useHomfoSurveyStore from '../../../store/context/useHomfoSurveyStore';
 import { QuestionForm,HomfoEditData } from '../../../store/type/homfoRecommend&request/interface';
 import { useNavigate } from 'react-router-dom';
 import SelectedForm from '../../../components/selectedForm';
@@ -10,10 +10,10 @@ interface SelectedProgressProps {
   setCount: React.Dispatch<React.SetStateAction<number>>;
   totalCount: number;
 }
-const SelectedHompoSurvey = (props: SelectedProgressProps) => {
+const SelectedHomfoSurvey = (props: SelectedProgressProps) => {
   const navigate = useNavigate();
-  const currentQuestion: QuestionForm = hompoQuestionList[props.count - 1];
-  const previousQuestion: QuestionForm = hompoQuestionList[props.count - 2];
+  const currentQuestion: QuestionForm = homfoQuestionList[props.count - 1];
+  const previousQuestion: QuestionForm = homfoQuestionList[props.count - 2];
   const [data, setData] = useState<HomfoEditData>({
     universityPeople: [],
     transports: [],
@@ -21,7 +21,7 @@ const SelectedHompoSurvey = (props: SelectedProgressProps) => {
     facilities: [],
   });
   const [filterValue,setFilterValue] = useState<{[key:string]:number[]}>({});
-  const {postHomfoRecommendInfo} = useHompoSurveyStore();
+  const {postHomfoRecommendInfo} = useHomfoSurveyStore();
   return (
     <div style={{marginTop:"10vh"}}>
       <SelectedForm currentQuestion={currentQuestion} previousQuestion={previousQuestion} mode={"time"} data={data} setData={setData} setFilterValue={setFilterValue}/>
@@ -31,10 +31,10 @@ const SelectedHompoSurvey = (props: SelectedProgressProps) => {
           if(props.count===1&&data[currentQuestion.question.type][0]===false){
             props.setCount(props.count+3);
           } else if(props.totalCount ===props.count) {
-            navigate('/mypage/hompo-recommended-result');
+            navigate('/mypage/homfo-recommended-result');
             postHomfoRecommendInfo(2, data, filterValue);
           } else{
-            if (props.count < hompoQuestionList.length) {
+            if (props.count < homfoQuestionList.length) {
               props.setCount(props.count + 1);
             }
           }
@@ -45,4 +45,4 @@ const SelectedHompoSurvey = (props: SelectedProgressProps) => {
   );
 };
 
-export default SelectedHompoSurvey;
+export default SelectedHomfoSurvey;
