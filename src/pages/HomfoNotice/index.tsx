60166442b09004f3ef6db5860a76217e8c5fc537
KANG DONGHA <knamuuu@naver.com>
2023-10-23 22:46:56 +0900

    Feat: 공지사항 페이지 배너 컴포넌트

--- src/pages/HomfoNotice/index.tsx
+++ src/pages/HomfoNotice/index.tsx
@@ -1,11 +1,26 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+
 import Header from '../../components/layout/header';
-import styles from './styles.module.scss'
-function HomfoNotice(){
-    return(
+
+import notice_icon from '../../assets/icons/home/notice_icon.svg';
+
+import styles from './styles.module.scss';
+
+function HomfoNotice() {
+    return (
         <div className={styles.container}>
-         <Header title="공지사항" color='white'/>
-         
+            <Header title="공지사항" color="white" />
+            <div className={styles.banner}>
+                <div className={styles.left}>
+                    <div>Hompo에서</div>
+                    <div className={styles.strong}>
+                        <strong>최신 공지사항 알려드립니다.</strong>
+                    </div>
+                </div>
+                <div className={styles.right}>
+                    <img src={notice_icon} alt="공지사항" />
+                </div>
+            </div>
         </div>
     );
 }
