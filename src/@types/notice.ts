25271119494fb02ef389e4c9f9c1612d7d0d596c
KANG DONGHA <knamuuu@naver.com>
2023-11-07 20:37:44 +0900

    Feat: 공지사항 리스트 api 연결

--- src/@types/notice.ts
+++ src/@types/notice.ts
@@ -1,6 +1,20 @@
 export interface INotice {
-    noticeId: number;
-    name: string;
-    isFixed: 'Y' | 'N';
-    createAt: string;
+    id: number;
+    name: string | null;
+    content: string;
+    writer: IWriter;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface IWriter {
+    userId: number;
+    userAccount: string;
+    userPhoneNum: string;
+    nickName: string;
+    gender: string;
+    dateOfBirth: string;
+    job: string;
+    role: string;
+    status: string;
 }
--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -6,12 +6,20 @@ export function formatDateTime(dateTimeString: string): string {
     const hours = dateTime.getHours();
     const minutes = dateTime.getMinutes();
     return `${year}년 ${month}월 ${day}일 ${hours}:${minutes}`;
-  }
+}
 export function formatDate(dateTimeString: string): string {
     const dateTime = new Date(dateTimeString);
     const year = dateTime.getFullYear();
     const month = dateTime.getMonth() + 1;
     const day = dateTime.getDate();
     return `${year}년 ${month}월 ${day}일`;
-  }
-  
+}
+
+export function formatDateWithComma(dateTimeString: string): string {
+    const dateTime = new Date(dateTimeString);
+    const year = dateTime.getFullYear();
+    const month = (dateTime.getMonth() + 1).toString().padStart(2, '0');
+    const day = dateTime.getDate().toString().padStart(2, '0');
+
+    return `${year}. ${month}. ${day}`;
+}
