ea33de644743e39f8c3a2713ada75aae12e939c7
yunmi099 <yunmi099@ajou.ac.kr>
2023-11-13 12:08:08 +0900

    feat

--- src/pages/RequestDocument/index.tsx
+++ src/pages/RequestDocument/index.tsx
@@ -26,6 +26,9 @@ function RequestDocument(){
                     {data?.realtor.agencyThumbnailDto.name}
                 </div>
                 <div>
+                    {data?.realtor.position !== null && `직책 : ${data?.realtor.position}`}
+                </div>
+                <div>
                     {data?.realtor.agencyThumbnailDto.type}&nbsp;{data?.agencyItem.agency.chairmanName}
                 </div>
                 <div>
@@ -67,27 +70,47 @@ function RequestDocument(){
             </div>
             <div className={styles.horizontalLine}></div>
             <div className={styles.title}>기본정보</div>
-            <div className={styles.itemInfo}>
-            <div style={{fontSize:'1.1em', marginTop: 5}}>{data?.agencyItem.item.name}</div>
-                <div>매물 유형  {data?.agencyItem.itemType}</div>
-            </div>
             <div className={styles.squareBox}>
                 <div  className={styles.agencyInfo}>
-                    <div>
-                        
-                    </div>
+                    <div style={{fontSize:'1.15em', marginTop: 5,color:"black"}}>{data?.agencyItem.item.name}</div>
+                    <div>방 개수 : {data?.agencyItem.item.roomNumber}</div>
+                    <div>전용 면적: {data?.agencyItem.item.exclusiveArea}</div>
+                    <div>공급 면적: {data?.agencyItem.item.supplyArea}</div>
+                    <div>매물 유형 : {data?.agencyItem.itemType}</div>
+                    <div>계약 형태 : {data?.agencyItem.contractTypes.data}</div>
+                    <div>월세 보증금: {data?.agencyItem.monthlyDeposit}</div>
+                    <div>월세: {data?.agencyItem.monthlyFee}</div>
+                    <div>전세 보증금: {data?.agencyItem.jeonseDeposit}</div>
+                    <div>관리비: {data?.agencyItem.maintenanceCost}</div>
+                    <div>대출유무/유형 : Y/{data?.agencyItem.loanType}</div>
+                    <div>입주시기 : {data?.agencyItem.moveInPeriod}</div>
                 </div>
-            </div>
+                </div>
             <div>
                 <div className={styles.title}>옵션</div>
-                <div className={styles.title}>기타 전달사항</div>
-                <div className={styles.title}>위치</div> 
 
+                <div style={{width: '100vw', height: '10vh', marginTop: 20}}>
+                    {
+                        data?.agencyItem.itemOptions.data.map((item)=>{
+                            return(<div className={styles.university}>{item.name}</div>)
+                        })
+                    }
+                </div>
+
+                <div className={styles.title}>기타 전달사항</div>
                 <div className={styles.squareBox}>
-                
+                    <div  className={styles.agencyInfo}>
+                        {data?.agencyItem.note}
+                    </div>
                 </div>
 
                 <div className={styles.title}>도로명 주소</div>
+                <div style={{width: '100vw', height: '10vh', marginTop: 20, marginLeft:'5%'}}>
+                    <div  className={styles.adress}>
+                        {data?.agencyItem.item.roadAddress} &nbsp;
+                        {data?.agencyItem.item.floor}(층)
+                    </div>
+                </div>
             </div>
 
         </div>
