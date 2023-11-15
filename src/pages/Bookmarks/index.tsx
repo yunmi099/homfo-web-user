2b27e50c2f20ce780d2f71d1f53b98e12afdf535
KANG DONGHA <knamuuu@naver.com>
2023-11-15 12:34:58 +0900

    Feat: bookmarks 페이지

---
+++ src/pages/Bookmarks/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../components/layout/header';
+
+export default function Bookmarks() {
+    return (
+        <div>
+            <Header title="즐겨찾기" color="white" />
+            Bookmarks
+        </div>
+    );
+}
