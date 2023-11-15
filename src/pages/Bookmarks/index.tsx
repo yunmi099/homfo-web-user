364e7c270b5ae522af1bf2199d38ed9d3244daee
KANG DONGHA <knamuuu@naver.com>
2023-11-15 12:48:16 +0900

    Feat: selector

--- src/pages/Bookmarks/index.tsx
+++ src/pages/Bookmarks/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/layout/header';
 
+import styles from './styles.module.scss';
+
 export default function Bookmarks() {
+    const [isArea, setIsArea] = useState<Boolean>(true);
+
     return (
         <div>
             <Header title="즐겨찾기" color="white" />
-            Bookmarks
+            <div className={styles.container}>
+                <div className={styles.selector}>
+                    <div
+                        className={`${isArea ? styles.active : styles.nonactive}`}
+                        onClick={() => setIsArea(true)}
+                    >
+                        관심 구역
+                    </div>
+                    <div
+                        className={`${!isArea ? styles.active : styles.nonactive}`}
+                        onClick={() => setIsArea(false)}
+                    >
+                        관심 상식
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
