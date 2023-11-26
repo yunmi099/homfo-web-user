035e85aeda530d54b73357fb56a31065c57da884
yunmi099 <yunmi099@ajou.ac.kr>
2023-11-27 05:00:48 +0900

    Feat: jwt token interceptor 적용

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,29 +1,39 @@
 import axios, { Method } from 'axios';
-import useUserStore from '../store/context/useUserStore';
 const SERVER_DEPOLY_URL = 'https://dev-server.homfo.co.kr/api';
+const SERVER_PRODUCTION_URL = 'https://prod-server.homfo.co.kr/api';
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    try {
+      if (token) {
+        config.headers.Authorization = token;
+      }
+      return config;
+    } catch (err) {
+      console.error("[_axios.interceptors.request] config : " + err);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+
 export const fetchFromApi = async (
     method: Method | undefined,
     url: string,
     data?: any,
   ): Promise<any> => {
     try {
-      let token = localStorage.getItem("token")
-      // alert("before passed token:"+token);
-      let headers = {
-        "Content-Type":'application/json',
-        "Authorization": 'Bearer eyJyZWdEYXRlIjoxNzAwOTg1NDkzOTk3LCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiVVNFUiIsInVzZXJJZCI6MzcsImFjY291bnQiOiJ5dW5taTA5OSIsImlhdCI6MTcwMDk4NTQ5MywiZXhwIjoxNzAyMTk1MDkzfQ.XTgZEDItZKnIE-J8gief0OQS4nLVZ2osbF3lKOhFy6E',
-      }
-      // alert("after ?passed token:"+headers.Authorization);
       const response = await axios({
         method,
-        url: SERVER_DEPOLY_URL + url,
+        url: SERVER_DEPOLY_URL+ url,        
         data,
-        headers: headers,
       });
-      console.log(response)
       return (response);
     } catch (err) {
-      console.log(err);
       throw err;
     }
   };
