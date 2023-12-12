99fc7c8047a39ff9c861a3607cccbb4fd06bb6ef
yunmi099 <yunmi099@ajou.ac.kr>
2023-12-12 21:57:32 +0900

    design: 마이페이지 디자인 적용

--- src/pages/Mypage/Main/index.tsx
+++ src/pages/Mypage/Main/index.tsx
@@ -5,7 +5,8 @@ import { useNavigate } from 'react-router-dom';
 import useUserStore from '../../../store/context/useUserStore';
 import BottomTab from '../../../components/layout/bottomtabs';
 import useHomfoSurveyStore from '../../../store/context/useHomfoSurveyStore';
-
+import profileImage from '../../../assets/hippo/profileImage.png'
+import arrow from '../../../assets/icons/bottomtab/mypage/arrow.png'
 function Mypage() {
     const navigate = useNavigate();
     const { userInfo } = useUserStore((state) => state);
@@ -13,13 +14,14 @@ function Mypage() {
     return (
         <div className={styles.container}>
             <Header title="마이페이지" back={false} />
-            <div className={styles.profileImg}></div>
-            <div>
+            <img src={profileImage} className={styles.profileImg}/>
+            <div className={styles.mention}>
                 <span>{userInfo.nickName}</span>님의 슬기로운
             </div>
-            <div>자취 생활을 늘 응원합니다!</div>
+            <div className={styles.welcome}>자취 생활을 늘 응원합니다!</div>
             <div className={styles.menuBox}>
                 <div
+                    className={styles.purpleRouting}
                     onClick={() =>
                         navigate(
                             result === null
@@ -30,13 +32,63 @@ function Mypage() {
                 >
                     홈포 추천 구역
                 </div>
-                <div onClick={() => navigate('/mypage/personalinfo')}>개인정보 및 수정</div>
-                <div onClick={() => navigate('/mypage/accountinfo')}>계정정보 및 수정</div>
-                <div onClick={() => navigate('/mypage/bookmarks')}>즐겨찾기</div>
-                <div onClick={() => navigate('/request-box')}>요청서 확인</div>
-                <div onClick={() => navigate('/mypage/setting')}>앱 설정</div>
-                <div onClick={() => navigate('/inquiry')}>문의하기</div>
-                <div onClick={() => navigate('/faq')}>FAQ</div>
+                <div 
+                    className={styles.purpleRouting}
+                    onClick={() => navigate('/mypage/personalinfo')}
+                >
+                    개인정보 및 수정
+                </div>
+                <div
+                    className={styles.purpleRouting}
+                    onClick={() => navigate('/mypage/accountinfo')}
+                >
+                    계정정보 및 수정
+                </div>
+                <div 
+                    className={styles.purpleRouting}
+                    onClick={() => navigate('/mypage/bookmarks')}
+                >
+                    즐겨찾기
+                </div>
+                <div 
+                    className={styles.purpleRouting}
+                    onClick={() => navigate('/request-box')}
+                >
+                    요청서 확인
+                </div>
+                <div 
+                    className={styles.routing}
+                    onClick={() => navigate('/mypage/setting')}
+                >
+                    앱 설정
+                    <img
+                        alt=""
+                        className={styles.arrow}
+                        src={arrow}
+                    />
+                </div>
+                <div 
+                    className={styles.routing}
+                    onClick={() => navigate('/inquiry')}
+                >
+                    문의하기
+                    <img
+                        alt=""
+                        className={styles.arrow}
+                        src={arrow}
+                    />
+                </div>
+                <div 
+                    className={styles.routing}
+                    onClick={() => navigate('/faq')}
+                >
+                    FAQ
+                    <img
+                        alt=""
+                        className={styles.arrow}
+                        src={arrow}
+                    />
+                </div>
             </div>
             <BottomTab />
         </div>
