e391ffc222592c08cbfc03ed0bc4dc26b3c600f8
yunmi099 <yunmi099@ajou.ac.kr>
2023-12-12 21:02:05 +0900

    design: 렌더링 이미지 수정

--- src/pages/HomfoRecommendResult/renderingPage/index.tsx
+++ src/pages/HomfoRecommendResult/renderingPage/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import styles from  './styles.module.scss'
-import renderingHippo from '../../../assets/hippo/homfoRenderingImage.png'
+import renderingVideo from '../../../assets/hippo/renderingVideo.gif'
 export default function Rendering() {
   return (
     <div className={styles.container}>
         <div className={styles.contents}>당신에게 <span>어울리는 구역</span>을<br/>찾고 있어요.</div>
         <div className={styles.smallContents}>잠시만 기다려 주세요.</div>
         <img
-          src={renderingHippo}
           className={styles.image}
           alt=""
+          src={renderingVideo}
         />
     </div>
   )
